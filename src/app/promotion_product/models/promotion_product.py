db20d0105accee41b5a4bf4bfaf6d300f7d0956c
woojin-an <dkswoojin@gmail.com>
2024-11-21 19:58:46 +0900

    💡 chore: Refactoring response type of the promotion

--- src/app/promotion_product/models/promotion_product.py
+++ src/app/promotion_product/models/promotion_product.py
@@ -1,13 +1,20 @@
+from enum import Enum
+
 from tortoise import fields
 
 from common.models.base_model import BaseModel
+
+
+class PromotionType(str, Enum):
+    BEST = "best"
+    MD_PICK = "md_pick"
 
 
 class PromotionProduct(BaseModel):
     product = fields.ForeignKeyField(
         "models.Product", related_name="promotion", on_delete=fields.CASCADE
     )  # type: ignore
-    promotion_type = fields.CharField(max_length=10)  # best, md_pick
+    promotion_type = fields.CharEnumField(PromotionType, max_length=10)   # best, md_pick
     is_active = fields.BooleanField(default=True)
 
     class Meta:
