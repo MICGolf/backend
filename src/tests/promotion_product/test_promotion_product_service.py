811dc224e94e3879beaa0ae0263ea3bfb11c2ec9
woojin-an <dkswoojin@gmail.com>
2024-11-25 18:07:46 +0900

    ✅ test: promotion_product Test Code 작성

--- src/tests/promotion_product/test_promotion_product_service.py
+++ src/tests/promotion_product/test_promotion_product_service.py
@@ -1,4 +1,7 @@
+import uuid
+
 from tortoise.contrib.test import TestCase
+from tortoise.exceptions import DoesNotExist
 
 from app.product.models.product import DiscountOption, Product
 from app.promotion_product.dtos.promotion_response import PromotionProductListResponse, PromotionProductResponse
@@ -44,3 +47,77 @@ class TestPromotionProductService(TestCase):
             assert isinstance(item, PromotionProductResponse)
         assert response.page == page
         assert response.size == size
+
+    async def test_add_promotion_products(self) -> None:
+        # Given
+        promotion_type = "md_pick"
+        is_active = True
+        product_name = self.product.name
+
+        # When
+        response = await PromotionProductService.add_promotion_products(
+            promotion_type=promotion_type, product_name=product_name, is_active=is_active
+        )
+
+        # Then
+        assert response.product_name == product_name
+        assert response.is_active == is_active
+        assert response.promotion_type == promotion_type
+
+    async def test_update_promotion_products(self) -> None:
+        # Given
+        product_id = self.product.id
+        promotion_type = self.promotion.promotion_type
+        is_active = self.promotion.is_active
+
+        # When
+        new_promotion_type = PromotionType.BEST
+        new_is_active = False
+        response = await PromotionProductService.update_promotion_products(
+            product_id=product_id,
+            promotion_type=promotion_type,
+            is_active=is_active,
+            new_promotion_type=new_promotion_type,
+            new_is_active=new_is_active,
+        )
+
+        # Then
+        assert response.promotion_type == new_promotion_type
+        assert response.is_active == new_is_active
+
+    async def test_update_promotion_product_does_not_exist(self) -> None:
+        # Given
+        invalid_product_id = int(uuid.uuid4())
+        promotion_type = "best"
+        is_active = True
+
+        # When
+        try:
+            await PromotionProductService.update_promotion_products(
+                product_id=invalid_product_id,
+                promotion_type=promotion_type,
+                is_active=is_active,
+                new_promotion_type=PromotionType.BEST,
+                new_is_active=False,
+            )
+        except DoesNotExist as e:
+            assert (
+                str(e)
+                == f"No PromotionProduct found with product_id={invalid_product_id}, promotion_type={promotion_type}"
+            )
+
+    async def test_delete_promotion_products(self) -> None:
+        # Given
+        product_id = self.product.id
+        promotion_type = self.promotion.promotion_type
+
+        # When
+        await PromotionProductService.delete_promotion_products(
+            product_id=product_id,
+            promotion_type=promotion_type,
+        )
+
+        # Then
+        # 해당 프로모션 제품이 삭제되었는지 확인
+        with self.assertRaises(DoesNotExist):
+            await PromotionProduct.get(product_id=product_id, promotion_type=promotion_type)
