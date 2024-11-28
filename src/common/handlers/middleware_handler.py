73a60f2d59dd5a87500bfd3a390aeae7df4ceb66
woojin-an <dkswoojin@gmail.com>
2024-11-28 14:53:40 +0900

    🚚 build: allow hosts 추가

--- src/common/handlers/middleware_handler.py
+++ src/common/handlers/middleware_handler.py
@@ -31,5 +31,5 @@ def attach_middleware_handlers(app: FastAPI) -> None:
     # app.add_middleware(CommonResponseMiddleware)
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts=["micgolf.kro.kr", "*.micgolf.kro.kr", "localhost", "211.188.61.243", "test"],
+        allowed_hosts=["micgolf.kro.kr", "*.micgolf.kro.kr", "localhost", "211.188.61.243", "test", "*"],
     )
