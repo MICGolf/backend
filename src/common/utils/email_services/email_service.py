10173c6629a4f8aaa19f0884f88d34215d1a1d7a
taewoo-dev <twin9774@gmail.com>
2024-11-28 13:30:01 +0900

    💡 chore: 오타수정

--- src/common/utils/email_services/email_service.py
+++ src/common/utils/email_services/email_service.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class EmailService(ABC):
     @abstractmethod
-    async def send_email(self, to: str, subject: str, message: str) -> ㅇict[str, str]:
+    async def send_email(self, to: str, subject: str, message: str) -> dict[str, str]:
         pass
