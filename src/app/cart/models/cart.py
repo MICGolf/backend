f8c658a7c85bbe8beff53404f91bcb30647a0753
woojin-an <dkswoojin@gmail.com>
2024-12-02 15:54:25 +0900

    ✨ feat: option과 FK 설정하여 옵션 별 수량, 재고 등 파악하도록 함

--- src/app/cart/models/cart.py
+++ src/app/cart/models/cart.py
@@ -1,13 +1,17 @@
 from tortoise import fields
 
+from app.product.models.product import Option
 from common.models.base_model import BaseModel
 
 
 class Cart(BaseModel):
     user = fields.ForeignKeyField("models.User", related_name="cart", on_delete=fields.CASCADE)  # type: ignore
     product = fields.ForeignKeyField("models.Product", related_name="cart_item", on_delete=fields.CASCADE)  # type: ignore
+    option: fields.ForeignKeyRelation["Option"] = fields.ForeignKeyField(
+        "models.Option", related_name="cart_option", on_delete=fields.CASCADE
+    )
     product_count = fields.IntField(default=1)
 
     class Meta:
         table = "cart"
-        unique_together = ("user", "product")  # 유니크 제약조건
+        unique_together = ("user", "product", "option")  # 유니크 제약조건
