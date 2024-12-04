6bd9fb2b17381ce35a7b96371ef3f7403ce07046
woojin-an <dkswoojin@gmail.com>
2024-12-04 18:41:26 +0900

    🐛 fix: option_id를 이용한 Cart CRUD 수정

--- src/app/cart/services/cart_services.py
+++ src/app/cart/services/cart_services.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from fastapi import HTTPException, status
-from tortoise.exceptions import DoesNotExist, ValidationError
+from tortoise.exceptions import DoesNotExist
 
 from app.cart.dtos.cart_response import CartItemResponse, CartResponse
 from app.cart.models.cart import Cart
@@ -14,6 +14,15 @@ class CartService:
         # 해당 유저의 모든 장바구니 가져오기 (product, option 관계 포함)
         cart = await Cart.filter(user_id=user_id).prefetch_related("product", "option", "option__images").all()
 
+        # option_id와 product_id 추출
+        option_ids = [item.option_id for item in cart]  # type: ignore
+        product_ids = [item.product_id for item in cart]  # type: ignore
+
+        # count_product 가져오기
+        stock = await CountProduct.filter(option_id__in=option_ids, product_id__in=product_ids).all()
+
+        count_product_map = {(cp.product_id, cp.option_id): cp.count for cp in stock}  # type: ignore
+
         # 장바구니가 없다면
         if not cart:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cart does not exist")
@@ -21,14 +30,21 @@ class CartService:
         # 각 장바구니 아이템을 CartItemResponse로 맵핑
         items: List[CartItemResponse] = [
             CartItemResponse(
+                cart_id=item.id,
                 user_id=item.user_id,  # type: ignore
                 product_id=item.product_id,  # type: ignore
+                option_id=item.option_id,  # type: ignore
+                product_code=item.product.product_code,
                 product_name=item.product.name,
-                product_count=item.product_count,
+                product_image_url=item.option.images[0].image_url if item.option.images else "",
                 product_color=item.option.color,
                 product_size=item.option.size,
-                product_price=float(item.product.price),
-                product_image_url=item.option.images[0].image_url if item.option.images else "",
+                product_amount=item.product_count,
+                product_stock=count_product_map.get((item.product_id, item.option_id), 0),  # type: ignore
+                origin_price=item.product.origin_price,
+                price=float(item.product.price),
+                discount=item.product.discount,
+                discount_option=item.product.discount_option,
             )
             for item in cart
         ]
@@ -43,16 +59,16 @@ class CartService:
         )
 
     @staticmethod
-    async def add_to_cart(
-        user_id: int, product_id: int, color: str, size: str, product_count: int = 1
-    ) -> CartItemResponse:
-        # 주어진 color와 size로 Option을 조회
-        option, stock_count = await Option.get_option_with_stock(product_id, color, size)
+    async def add_to_cart(user_id: int, product_id: int, option_id: int, product_count: int = 1) -> CartItemResponse:
+        # Option을 조회
+        option = await Option.get(id=option_id)
 
         # 재고 초과 확인
-        if product_count > stock_count:
+        stock = await CountProduct.filter(option_id=option_id, product_id=product_id).first()
+        if not stock or product_count > stock.count:
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Requested quantity exceeds available stock"
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Requested quantity exceeds available stock.",
             )
 
         # 유저 장바구니에서 동일한 상품과 옵션이 있는지 확인
@@ -64,7 +80,7 @@ class CartService:
 
         # 해당 장바구니에 동일한 상품과 옵션이 있을 때 (수량 업데이트)
         if user_cart:
-            if user_cart.product_count + product_count > stock_count:
+            if user_cart.product_count + product_count > stock.count:
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
                     detail="Requested quantity exceeds available stock.",
@@ -74,38 +90,51 @@ class CartService:
             await user_cart.save()
 
             return CartItemResponse(
-                user_id=user_cart.user,
-                product_id=user_cart.product,
+                cart_id=user_cart.id,
+                user_id=user_cart.user_id,  # type: ignore
+                product_id=user_cart.product_id,  # type: ignore
+                option_id=user_cart.option_id,  # type: ignore
+                product_code=user_cart.product.product_code,
                 product_name=user_cart.product.name,
-                product_count=user_cart.product_count,
+                product_image_url=user_cart.option.images[0].image_url if user_cart.option.images else "",
                 product_color=user_cart.option.color,
                 product_size=user_cart.option.size,
-                product_price=float(user_cart.product.price),
-                product_image_url=user_cart.option.images[0].image_url if user_cart.option.images else "",
+                product_amount=user_cart.product_count,
+                product_stock=stock.count,
+                origin_price=user_cart.product.origin_price,
+                price=float(user_cart.product.price),
+                discount=user_cart.product.discount,
+                discount_option=user_cart.product.discount_option,
             )
 
         # 장바구니에 동일한 상품과 옵션이 없을 때 (새로운 상품 추가)
         new_cart = await Cart.create(
             user_id=user_id, product_id=product_id, option_id=option.id, product_count=product_count
         )
+        new_cart = await Cart.get(id=new_cart.id).prefetch_related("product", "option", "option__images")
         return CartItemResponse(
-            user_id=new_cart.user,
-            product_id=new_cart.product,
+            cart_id=new_cart.id,
+            user_id=new_cart.user_id,  # type: ignore
+            product_id=new_cart.product_id,  # type: ignore
+            option_id=new_cart.option_id,  # type: ignore
+            product_code=new_cart.product.product_code,
             product_name=new_cart.product.name,
-            product_count=new_cart.product_count,
+            product_image_url=new_cart.option.images[0].image_url if new_cart.option.images else "",
             product_color=new_cart.option.color,
             product_size=new_cart.option.size,
-            product_price=float(new_cart.product.price),
-            product_image_url=new_cart.option.images[0].image_url if new_cart.option.images else "",
+            product_amount=new_cart.product_count,
+            product_stock=stock.count,
+            origin_price=new_cart.product.origin_price,
+            price=float(new_cart.product.price),
+            discount=new_cart.product.discount,
+            discount_option=new_cart.product.discount_option,
         )
 
     @staticmethod
-    async def update_cart(user_id: int, product_id: int, color: str, size: str, product_count: int) -> CartItemResponse:
-        # Option과 재고를 클래스 메서드를 사용해 가져오기
-        option, stock_count = await Option.get_option_with_stock(product_id, color, size)
-
-        # 재고 초과 확인
-        if product_count > stock_count:
+    async def update_cart(user_id: int, product_id: int, option_id: int, product_count: int) -> CartItemResponse:
+        # Option과 재고를 조회
+        stock = await CountProduct.filter(option_id=option_id, product_id=product_id).first()
+        if not stock or product_count > stock.count:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Requested quantity exceeds available stock.",
@@ -113,7 +142,7 @@ class CartService:
 
         # 유저의 장바구니에서 해당 상품과 옵션 조회
         try:
-            user_cart = await Cart.get(user_id=user_id, product_id=product_id, option_id=option.id).prefetch_related(
+            user_cart = await Cart.get(user_id=user_id, product_id=product_id, option_id=option_id).prefetch_related(
                 "product", "option", "option__images"
             )
         except DoesNotExist:
@@ -127,30 +156,28 @@ class CartService:
         await user_cart.save()
 
         return CartItemResponse(
-            user_id=user_cart.user,
-            product_id=user_cart.product,
+            cart_id=user_cart.id,
+            user_id=user_cart.user_id,  # type: ignore
+            product_id=user_cart.product_id,  # type: ignore
+            option_id=user_cart.option_id,  # type: ignore
+            product_code=user_cart.product.product_code,
             product_name=user_cart.product.name,
-            product_count=user_cart.product_count,
+            product_image_url=user_cart.option.images[0].image_url if user_cart.option.images else "",
             product_color=user_cart.option.color,
             product_size=user_cart.option.size,
-            product_price=float(user_cart.product.price),
-            product_image_url=user_cart.option.images[0].image_url if user_cart.option.images else "",
+            product_amount=user_cart.product_count,
+            product_stock=stock.count,
+            origin_price=user_cart.product.origin_price,
+            price=float(user_cart.product.price),
+            discount=user_cart.product.discount,
+            discount_option=user_cart.product.discount_option,
         )
 
     @staticmethod
-    async def delete_cart(user_id: int, product_id: int, color: str, size: str) -> None:
-        try:
-            # 주어진 product_id, color, size에 해당하는 Option 조회
-            option = await Option.get(product_id=product_id, color=color, size=size)
-        except DoesNotExist:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="The specified option does not exist.",
-            )
-
+    async def delete_cart(user_id: int, product_id: int, option_id: int) -> None:
         try:
             # 유저의 장바구니에서 해당 상품과 옵션 조회
-            user_cart = await Cart.get(user_id=user_id, product_id=product_id, option_id=option.id)
+            user_cart = await Cart.get(user_id=user_id, product_id=product_id, option_id=option_id)
         except DoesNotExist:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
