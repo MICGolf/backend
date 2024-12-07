feaa63f4315e695cf9d44e8bb235ab0c3737a0b2
woojin-an <dkswoojin@gmail.com>
2024-12-07 16:54:24 +0900

    ✨ feat: 장바구니 추가 시 color와 size를 받도록 수정 (Request model 작성)

--- src/app/cart/dtos/cart_request.py
+++ src/app/cart/dtos/cart_request.py
@@ -5,3 +5,10 @@ class CartItemRequest(BaseModel):
     product_id: int = Field(..., description="상품 ID")
     option_id: int = Field(..., description="옵션 ID")
     product_count: int = Field(description="상품 수량", ge=1)
+
+
+class CartItemAddRequest(BaseModel):
+    product_id: int = Field(..., description="상품 ID")
+    color: str = Field(..., description="색상")
+    size: str = Field(..., description="사이즈")
+    product_count: int = Field(..., description="상품 수량", ge=1)
