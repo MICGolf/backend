568202d9c501dc6a29a16dec0ba5f74b65bcc4b5
woojin-an <dkswoojin@gmail.com>
2024-12-07 17:25:14 +0900

    🚚 build: PR이 Merge 될 때 자동으로 서버 reload 되도록 수정

--- src/common/github_webhook/github_webhook.py
+++ src/common/github_webhook/github_webhook.py
@@ -13,9 +13,26 @@ async def webhook(request: Request) -> dict[str, str]:
     if payload.get("ref") == "refs/heads/develop":
         # Git pull 명령 실행
         repo_path = "/root/backend/src"  # 서버의 레포지토리 경로
+        process_name = "gunicorn"
+
         try:
+            # git pull 명령어 실행
             subprocess.run(["git", "-C", repo_path, "pull", "origin", "develop"], check=True)
-            return {"message": "Git pull executed successfully"}
+
+            # server PID 가져오기
+            result = subprocess.run(
+                ["pgrep", "-f", process_name],
+                check=True,
+                capture_output=True,
+                text=True,
+            )
+            pid = result.stdout.strip().splitlines()[0]     # 첫번째 PID 가져오기
+
+            # HUP 신호 보내기
+            subprocess.run(["kill", "-HUP", pid], check=True)
+
+            return {"message": "Git pull & server reload executed successfully"}
+
         except subprocess.CalledProcessError as e:
             return {"error": f"Git pull failed: {e}"}
 
